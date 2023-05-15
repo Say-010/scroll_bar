941ac6e0fcec7faed721bc5947e5b1fa1134d0e4
Say-010 <sagarsahu0110@gmail.com>
2023-05-16 00:48:29 +0530

    message

---
+++ src/component/scrollbar.js
@@ -0,0 +1,60 @@
+export default function Scroller(){
+    return(
+        
+ <>
+<h1>   scroller that show @ scrolling </h1>
+
+<div className='wrapper'>
+
+
+
+<div className='card'> 
+<img src="https://images.pexels.com/photos/997217/pexels-photo-997217.jpeg?auto=compress&cs=tinysrgb&w=200&h=200"></img>
+ <p> sagar <br/> Trainee at functionup</p> 
+ </div>
+
+ <div className='card'> 
+ <img src="https://images.pexels.com/photos/997217/pexels-photo-997217.jpeg?auto=compress&cs=tinysrgb&w=200&h=200"></img>
+ <p>sagar s <br/> Trainee at functionup</p> </div>
+
+
+<div className='card'> 
+<p>sourabh s <br/> Trainee @ functionup</p> 
+</div>
+
+ <div className='card'> 
+ <img src="https://images.pexels.com/photos/997217/pexels-photo-997217.jpeg?auto=compress&cs=tinysrgb&w=200&h=200"></img> 
+  <p>harshit sahu <br/> Credit officier @ savantra</p>  </div>
+
+   
+<div className='card'>
+     <img src="https://images.pexels.com/photos/997217/pexels-photo-997217.jpeg?auto=compress&cs=tinysrgb&w=200&h=200"></img> <p>Deva<br/> Trainee @ functionup</p> </div>
+<div className='card'>
+     <img src="https://images.pexels.com/photos/997217/pexels-photo-997217.jpeg?auto=compress&cs=tinysrgb&w=200&h=200"></img> <p>Anurag <br/> Trainee @ functionup</p>  </div>
+<div className='card'>
+    <img src="https://images.pexels.com/photos/997217/pexels-photo-997217.jpeg?auto=compress&cs=tinysrgb&w=200&h=200"></img> <p> Anoop <br/> Trainee @ functionup </p> </div> 
+<div className='card'>
+     <img src="https://images.pexels.com/photos/997217/pexels-photo-997217.jpeg?auto=compress&cs=tinysrgb&w=200&h=200"></img><p>Sarfraz <br/> Trainee @ functionup</p>  </div> 
+<div className='card'>
+    <img src="https://images.pexels.com/photos/997217/pexels-photo-997217.jpeg?auto=compress&cs=tinysrgb&w=200&h=200"></img> <p>Zeeshan <br/> Trainee @ functionup</p> </div> 
+<div className='card'>
+    <img src="https://images.pexels.com/photos/997217/pexels-photo-997217.jpeg?auto=compress&cs=tinysrgb&w=200&h=200"></img> <p> Amit Kumar Sahu  <br/> lead SDET UST </p> </div> 
+
+ </div>
+   
+   
+ </>
+
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
